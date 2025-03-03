b5229ac568266c323c51a415a6b6ce6de5f51cb3
Richard <mukechiwarichard@gmail.com>
2025-03-04 01:24:17 +0200

    model trainer improvement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from credit_risk import logger
 from credit_risk.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline   
 from credit_risk.pipeline.stage_02_data_validation import DataValidationTrainingPipeline    
 from credit_risk.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline    
+from credit_risk.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -36,3 +37,12 @@ except Exception as e:
     logger.exception(e) 
     raise e
 
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = ModelTrainerTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x========x")  
+except Exception as e:
+    logger.exception(e) 
+    raise e
--- src/credit_risk/config/configuration.py
+++ src/credit_risk/config/configuration.py
@@ -3,7 +3,8 @@ from credit_risk.constants import *
 from credit_risk.utils.common import read_yaml, create_directories 
 from credit_risk.entity.config_entity import (DataIngestionConfig,
                                               DataValidationConfig,
-                                              DataTransformationConfig)
+                                              DataTransformationConfig,
+                                              ModelTrainerConfig)
 
 #creating a Configuration class
 class ConfigurationManager:
@@ -57,3 +58,22 @@ class ConfigurationManager:
         )
         
         return data_transformation_config
+    
+    def get_model_trainer_config(self)->ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.LinearRegression
+        schema = self.schema.TARGET_COLUMN
+        
+        create_directories([config.root_dir])
+        
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name = config.model_name,
+            fit_intercept=params.fit_intercept,
+            n_jobs = params.n_jobs,
+            target_column=schema.name
+        )
+        
+        return model_trainer_config
