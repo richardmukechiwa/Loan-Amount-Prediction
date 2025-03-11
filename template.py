5911bbff10e20088c384ff44d6f408a51060ed86
Richard <mukechiwarichard@gmail.com>
2025-03-11 21:45:46 +0200

    code update

--- template.py
+++ template.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
 project_name = "credit_risk"
 
 list_of_files = [
+    
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
