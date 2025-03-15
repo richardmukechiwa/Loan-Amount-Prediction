1837dde0d7e882cfbad97774cb9096e82b796174
Richard <mukechiwarichard@gmail.com>
2025-03-16 00:10:35 +0200

    update

--- src/credit_risk/pipeline/prediction.py
+++ src/credit_risk/pipeline/prediction.py
@@ -1,15 +1,19 @@
 import joblib
-import numpy as np 
-import pandas as pd 
+import numpy as np
 from pathlib import Path
 
 class PredictionPipeline:
     def __init__(self):
-        self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
+        self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))  # Load trained model
         
+        # Load the saved ColumnTransformer
+        self.preprocessor = joblib.load(Path("artifacts/model_trainer/preprocessor.joblib"))
+
+    def predict(self, raw_data):
+        # Apply the saved preprocessor to incoming raw data
+        transformed_data = self.preprocessor.transform(raw_data)
         
-        
-    def predict(self, data):
-        prediction = self.model.predict(data)
-        
-        return prediction  
+        # Predict on transformed data
+        scaled_prediction = self.model.predict(transformed_data)
+        return scaled_prediction
+
