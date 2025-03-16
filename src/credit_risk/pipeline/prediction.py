a79fcf93fa546d8659a05baa38665ebcc373c904
Richard <mukechiwarichard@gmail.com>
2025-03-16 23:17:40 +0200

    update

--- src/credit_risk/pipeline/prediction.py
+++ src/credit_risk/pipeline/prediction.py
@@ -1,17 +1,32 @@
+from pathlib import Path
 import joblib
 import numpy as np
-from pathlib import Path
+
 
 class PredictionPipeline:
     def __init__(self):
+        self.ohe = joblib.load(Path("artifacts/model_trainer/onehotencoder.joblib"))
+         
+        self.scaler = joblib.load(Path("artifacts/model_trainer/preprocessor.joblib"))
         self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))  # Load trained model
         
-        # Load the saved ColumnTransformer
-        self.preprocessor = joblib.load(Path("artifacts/model_trainer/preprocessor.joblib"))
-
     def predict(self, raw_data):
-        # Apply the saved preprocessor to incoming raw data
-        transformed_data = self.preprocessor.transform(raw_data)
+        # Apply the saved ohe and scaler on the raw data
+        data = self.ohe.transform(raw_data)
+    
+        data = self.scaler.transform(data)
+    
+        #make prediction using the loaded model and return unscaled predictions
+        predictions = self.model.predict(data)
+    
+        #return unscaled predictions
+        return predictions
+        
+        
+        
+        
+        
+        
         
         # Predict on transformed data
         scaled_prediction = self.model.predict(transformed_data)
